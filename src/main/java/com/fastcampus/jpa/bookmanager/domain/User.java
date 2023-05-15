c563680bc422a4acb004e4e9c308990645207024
khsrla9806 <kimhunsope@naver.com>
2023-05-15 15:57:07 +0900

    feat: User Entity 클래스 생성

---
+++ src/main/java/com/fastcampus/jpa/bookmanager/domain/User.java
@@ -0,0 +1,26 @@
+package com.fastcampus.jpa.bookmanager.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String name;
+    @NonNull
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
