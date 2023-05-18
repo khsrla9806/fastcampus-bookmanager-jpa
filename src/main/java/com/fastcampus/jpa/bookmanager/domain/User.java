ee707d6d76398b5dd2d44a36c7b0d3305c4deba7
khsrla9806 <kimhunsope@naver.com>
2023-05-18 17:52:10 +0900

    refactor: JpaAuditing 기능을 사용하여 기존 코드 변경
    - @EnableJpaAuditing을 추가
    - @CreatedDate, @LastModifiedDate를 각 Entity의 createdAt, updatedAt에 추가
    - @EntityListeners에 AuditingEntityListener.class를 추가

--- src/main/java/com/fastcampus/jpa/bookmanager/domain/User.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/domain/User.java
@@ -1,6 +1,9 @@
 package com.fastcampus.jpa.bookmanager.domain;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -11,7 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 @Entity
-@EntityListeners(value = { MyEntityListener.class, UserEntityListener.class })
+@EntityListeners(value = { AuditingEntityListener.class, UserEntityListener.class })
 @Table(name = "user", indexes = {@Index(columnList = "name")}, uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
 public class User implements Auditable {
     @Id
@@ -24,7 +27,9 @@ public class User implements Auditable {
     @Enumerated(value = EnumType.STRING) // default가 Ordinal
     private Gender gender;
     @Column(updatable = false) // update 쿼리에서 제외
+    @CreatedDate
     private LocalDateTime createdAt;
+    @LastModifiedDate
     private LocalDateTime updatedAt;
     @Transient // 영속 대상에서 제외
     private String testData;
--- src/main/java/com/fastcampus/jpa/bookmanager/domain/UserHistory.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/domain/UserHistory.java
@@ -2,17 +2,17 @@ package com.fastcampus.jpa.bookmanager.domain;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @Entity
-@EntityListeners(value = MyEntityListener.class)
+@EntityListeners(value = AuditingEntityListener.class)
 public class UserHistory implements Auditable {
     @Id
     @GeneratedValue
@@ -20,6 +20,9 @@ public class UserHistory implements Auditable {
     private Long userId;
     private String name;
     private String email;
+    @Column(updatable = false)
+    @CreatedDate
     private LocalDateTime createdAt;
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
